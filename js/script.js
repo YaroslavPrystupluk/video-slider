887f23845a09f6f87cbc9b75f1b3ffebaa165a25
Yaroslav Prystupliuk <tyucha_05@ukr.net>
2024-02-08 11:47:43 +0200

    add video and avatar slider

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+const video = document.querySelector("#vimeo-player");
+
 const swiper = new Swiper(".swiper", {
   direction: "horizontal",
   loop: true,
@@ -13,99 +15,66 @@ const swiper = new Swiper(".swiper", {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
-
 });
 
-
-// const getPathVideoMiniature = async () => {
-// 	try {
-//     const response = await fetch(
-//       "https://api.vimeo.com/videos/824804225?fields=metadata.connections.pictures.uri",
-//       {
-//         headers: {
-//           Authorization: "Bearer 9338e757510aad82573d9bd596e6931e",
-//           Accept: "application/vnd.vimeo.*+json;version=3.4",
-//         },
-//       }
-//     );
-//     if (!response.ok) {
-//       throw new Error("Failed to fetch video details");
-//     }
-
-//     const urlVideoMiniature = await response.json();
-//     const thumbnailUrl = urlVideoMiniature.metadata.connections.pictures.uri;
-
-//     return thumbnailUrl;
-
-//   } catch (error) {
-//     console.error("Error fetching video thumbnail:", error);
-//     return null;
-//   }
-// };
-
-// const getVideoMiniature = async () => {
-// 	const url = await getPathVideoMiniature();
-// 	console.log(url);
-//   const data = await fetch(`https://api.vimeo.com/videos/824804225/pictures`, {
-//     method: "POST",
-//     headers: {
-//       Authorization: "Bearer 9338e757510aad82573d9bd596e6931e",
-//       Accept: "application/vnd.vimeo.*+json;version=3.4",
-//       "Content-Type": "application/json",
-//     },
-//   });
-//   let res = await data.json();
-//   console.log(res);
-//   return res;
-// };
-
-// getVideoMiniature();
-
-// const videoPlayer = new Vimeo.Player("myVideo");
-// videoPlayer.on("play", function () {
-//   console.log("Played the video");
-// });
-
-
-const getUrlVideo = async () => {
+const getPathVideoMiniature = async () => {
   try {
     const response = await fetch("https://api.vimeo.com/videos/824804225", {
       headers: {
         Authorization: "Bearer 9338e757510aad82573d9bd596e6931e",
         Accept: "application/vnd.vimeo.*+json;version=3.4",
+        "Content-Type": "image/jpeg, image/png, image/gif",
       },
     });
     if (!response.ok) {
       throw new Error("Failed to fetch video details");
     }
 
-    const urlVideo = await response.json();
+    const urlVideoMiniature = await response.json();
+    const avatarUrl = urlVideoMiniature.pictures.sizes[2].link;
 
-    const video = document.querySelector("#vimeo-player");
-    video.setAttribute("data-vimeo-url", urlVideo.link);
-	 new Vimeo.Player("vimeo-player", {
-     url: urlVideo.link,
-     width: 640,
-     autoplay: false,
-   });
+    const imgs = document.querySelectorAll(".thumbnai");
+    imgs.forEach((img) => (img.src = avatarUrl));
 
+    return urlVideoMiniature;
+	 
   } catch (error) {
     console.error("Error fetching video thumbnail:", error);
     return null;
   }
 };
 
+const getUrlVideo = async () => {
+  const urlVideo = await getPathVideoMiniature();
+  const player = iniVideoPlayer(urlVideo.link);
+
+  iniVideoPlayer(urlVideo.link);
+  setAtrVimeoUrl(urlVideo.link, video);
+  btnClose(player, video);
+};
 getUrlVideo();
 
-
-function contentSlide() {
-  document.querySelectorAll(".swiper-slide").forEach(function (element) {
-    const content = element.textContent;
-	 
-    element.addEventListener("click", function () {
-      document.querySelector("#par").textContent = content
-    });
+const iniVideoPlayer = (url) => {
+  const player = new Vimeo.Player("vimeo-player", {
+    url,
+    width: 640,
+    autoplay: true,
+    loop: false,
   });
-}
+  return player;
+};
 
-contentSlide();
+const btnClose = (player, video) => {
+  document.querySelector(".btn-close").addEventListener("click", () => {
+    video.removeAttribute("data-vimeo-url");
+    player.pause();
+  });
+};
+
+const setAtrVimeoUrl = (url, video) => {
+document.querySelectorAll(".swiper-slide").forEach(function (element) {
+  element.addEventListener("click", function () {
+    video.setAttribute("data-vimeo-url", url);
+  });
+});
+};
