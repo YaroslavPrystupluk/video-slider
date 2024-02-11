267392f3631a8c19e8db09ecc6da819d707781f2
Iaroslav05 <tyucha_05@ukr.net>
2024-02-11 20:13:26 +0200

    add pag video

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
 const video = document.querySelector("#vimeo-player");
+const container = document.querySelector(".pagination-video");
+const images = document.querySelectorAll(".thumbnail");
+const modal = document.querySelector(".modal");
 const TOKEN = "9338e757510aad82573d9bd596e6931e";
 const videoId = "824804225";
 
@@ -10,7 +13,8 @@ const swiper = new Swiper(".swiper", {
   spaceBetween: 1,
 
   pagination: {
-    el: ".swiper-pagination-main",
+    el: ".swiper-pagination",
+    clickable: true,
   },
 
   navigation: {
@@ -33,11 +37,12 @@ const getPathVideoMiniature = async () => {
     }
 
     const urlVideoMiniature = await response.json();
-    const avatarUrl = urlVideoMiniature.pictures.sizes[2].link;
-
-    const imgs = document.querySelectorAll(".thumbnai");
-    imgs.forEach((img) => (img.src = avatarUrl));
-
+    const avatarUrl = urlVideoMiniature.pictures.base_link;
+    images.forEach((img) => {
+      img.src = avatarUrl;
+      img.setAttribute("data-url", urlVideoMiniature.link);
+    });
+    createPaginations(urlVideoMiniature.link);
     return urlVideoMiniature;
   } catch (error) {
     console.error("Error fetching video thumbnail:", error);
@@ -46,35 +51,65 @@ const getPathVideoMiniature = async () => {
 };
 
 const getUrlVideo = async () => {
-  const urlVideo = await getPathVideoMiniature();
-  startVideoPlayer(urlVideo.link, video);
-  btnClose(urlVideo.link, video);
+  await getPathVideoMiniature();
+  paginations(video);
 };
 getUrlVideo();
 
-const iniVideoPlayer = (url) => {
-  const player = new Vimeo.Player("vimeo-player", {
-    url,
+const createPaginations = (url) => {
+  const totalSlides = swiper.slides.length;
+  const pagination = document.createElement("ul");
+  pagination.classList.add("pagination-wrapper");
+  container.append(pagination);
+  for (let i = 0; i < totalSlides; i++) {
+    const paginationItem = document.createElement("li");
+    paginationItem.classList.add("pagination-item");
+    paginationItem.setAttribute("data-path", url);
+    pagination.append(paginationItem);
+  }
+};
+
+const iniVideoPlayer = async () => {
+  const player = await new Vimeo.Player("vimeo-player", {
     width: 550,
     autoplay: true,
-
   });
   return player;
 };
 
-const btnClose = (url, video) => {
-  document.querySelector(".btn-close").addEventListener("click", () => {
-    const player = iniVideoPlayer(url);
+const btnClose = (video) => {
+  document.querySelector(".btn-close").addEventListener("click", async () => {
+    const player = await iniVideoPlayer();
     video.removeAttribute("data-vimeo-url");
-    player.destroy();
+    await player.destroy();
   });
 };
+btnClose(video);
 
-const startVideoPlayer = (url, video) => {
-  document.querySelectorAll(".swiper-slide").forEach(function (element) {
-    element.addEventListener("click", function () {
-      video.setAttribute("data-vimeo-url", url);
-      iniVideoPlayer(url);
+const startVideoPlayer = (video) => {
+  images.forEach((image) => {
+    image.addEventListener("click", async () => {
+      const currentUrl = image.getAttribute("data-url");
+      video.setAttribute("data-vimeo-url", currentUrl);
+      iniVideoPlayer();
     });
   });
 };
+startVideoPlayer(video);
+
+const paginations = (video) => {
+  const buttonsPag = document.querySelectorAll(".pagination-item");
+  buttonsPag.forEach((buttonPag) => {
+    buttonPag.addEventListener("click", async () => {
+      video.removeAttribute("data-vimeo-url");
+      const newUrl = buttonPag.getAttribute("data-path");
+      const player = await iniVideoPlayer();
+      await player.destroy().then(() => {
+        video.setAttribute("data-vimeo-url", newUrl);
+        startVideoPlayer(video);
+        iniVideoPlayer();
+      });
+    });
+  });
+};
+paginations(video);
